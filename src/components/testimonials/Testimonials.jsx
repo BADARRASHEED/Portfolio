2e157a6bc7e122ebb73c32c007fe4060c65c1c01
Badar Rasheed Butt <211370113@gift.edu.pk>
2023-11-03 01:40:07 +0500

    Testimonials Started

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./testimonials.css";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h2>Testimonials</h2>
+    </div>
+  );
+};
+
+export default Testimonials;
