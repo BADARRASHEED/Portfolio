70b336a94fcc009464a6c844d5ef8fcbd9a2ecde
Badar Rasheed Butt <211370113@gift.edu.pk>
2023-11-03 15:46:37 +0500

    Contact Section start

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Services />
         <Qualification />
         <Testimonials />
+        <Contact />
       </main>
     </>
   );
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import Backend from "./Backend";
 
 const Skills = () => {
   return (
-    <section className="skills section" id="section">
+    <section className="skills section" id="skills">
       <h2 className="section_title">Skills</h2>
       <span className="section_subtitle">My technical level</span>
 
