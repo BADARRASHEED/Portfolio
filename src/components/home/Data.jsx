0fd7a598eded0001cd2c9cf2ae093b6e2b21ce64
Badar Rasheed Butt <98841403+BADARRASHEED@users.noreply.github.com>
2024-03-06 11:05:24 +0500

    code added

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ const Data = () => {
   return (
     <div className="home_data">
       <h1 className="home_title">
-        Badar Butt
+        Badar Butt Love Khadija
         <svg
           width="36"
           height="36"
