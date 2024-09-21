c33360f09d3d4261630f37a8406f5aeb8a04230c
Badar Rasheed Butt <98841403+BADARRASHEED@users.noreply.github.com>
2024-09-21 14:27:00 +0500

    added

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -74,13 +74,13 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="qualification_title">Web Development</h3>
+                <h3 className="qualification_title">Python Developer</h3>
 
                 <span className="qualification_subtitle">
                   Gift - University
                 </span>
                 <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>2022 - Present
+                  <i className="uil uil-calendar-alt"></i>2023 - Present
                 </div>
               </div>
             </div>
@@ -93,7 +93,7 @@ const Qualification = () => {
                   Gift - University
                 </span>
                 <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>2022 - Present
+                  <i className="uil uil-calendar-alt"></i>2024 - Present
                 </div>
               </div>
 
@@ -112,13 +112,13 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="qualification_title">Information Security</h3>
+                <h3 className="qualification_title">Deep Learning</h3>
 
                 <span className="qualification_subtitle">
                   Gift - University
                 </span>
                 <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>2022 - Present
+                  <i className="uil uil-calendar-alt"></i>2024 - Present
                 </div>
               </div>
             </div>
@@ -133,13 +133,13 @@ const Qualification = () => {
           >
             <div className="qualification_data">
               <div>
-                <h3 className="qualification_title">Web Developer</h3>
+                <h3 className="qualification_title">Power BI Expert</h3>
 
                 <span className="qualification_subtitle">
                   Miracle - Pakistan
                 </span>
                 <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>2017 - Present
+                  <i className="uil uil-calendar-alt"></i>2014 - Present
                 </div>
               </div>
 
